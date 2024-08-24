987b4ecf71432e053a521e39b2720ce7297bb2cb
gauravraj54646 <gauravraj54646@gmail.com>
2024-08-24 13:42:04 +0530

    first lets push

---
+++ Backend/controllers/chatControllers.js
@@ -0,0 +1,9 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import url, { fileURLToPath } from "url";
+import ImageKit from "imagekit";
+import mongoose from "mongoose";
+import Chat from "./models/chat.js";
+import UserChats from "./models/userChats.js";
+import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
